ed2b1155de66dbc5eacd9236c720cc85e59634dc
satyakiraj <satyakithecoder@gmail.com>
2026-03-28 21:08:35 +0530

    first commit

---
+++ GamePanel.java
@@ -0,0 +1,32 @@
+//write code adhering to OOPs principles like abstraction, polymorphism etc.
+import java.awt.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel
+{
+   /*
+   Code should always wriiten like a class whose object is going to be created in other class.
+   */
+    GamePanel(){
+       int pw = 800, ph = 700, btnWidth = 175, btnHeight = 50, x, y;
+       this.setBackground(Color.RED);
+       this.setPreferredSize(new Dimension(pw, ph)); // w * h (int input)
+       JButton start = new JButton("START");
+       start.setForeground(Color.WHITE);
+       start.setBackground(Color.GREEN);        
+       // Optional: Prevents the button from being 'focusable' at all
+       // (Useful for games where you want the keyboard to control the player, not the UI)
+      start.setFocusable(false);
+      /* 
+       Centering Button
+       Java has a default layout manager which is this.setLayout(). Either we can disable it or use Layouts like 
+       this.setLayout(new GridBagLayout());
+       we will use the formula to calculate it as given below.
+      */
+      this.setLayout(null);
+      x = (pw - btnWidth)/2;
+      y = (ph - btnHeight)/2;
+      start.setBounds(x, y, btnWidth, btnHeight); // x and y set the position and other set dimensions of the button.
+      this.add(start);
+    }
+}
